4c87d2b8ae73cbc51245b066b53a5908b660ae7e
Muhammad Hasnain Raza <157500053+raza7869999@users.noreply.github.com>
2024-03-26 11:34:24 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+Q:1
+
+var age = ("I am 15 years old" )
+console.log (age)
+alert("I am 15 years old")
+
+Q:2
+
+var numberOfVisitor = ("You have visited this site 14 times")
+console.log (numberOfVisitor)
+alert ("You have visited this site 14 times")
+
+Q:3 
+
+var birthYear = (1990)
+console.log (birthYear )
+
+Q:4
+var visitorName = ("Jhon Done ")
+console.log ("Visitor Name", visitorName )
+var ProductTittle = ("5T-shrit(s) ")
+console.log ( " Product Tittle", ProductTittle )
+var quantity = (100)
+console.log ( "Quantity",quantity )
